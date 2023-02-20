2a1e8b67bb00b1f94907d70812bf0f312bee9556
gyuho <kyuhokim12@gmail.com>
2023-02-20 16:34:20 +0900

    Update TodoApplication.kt

--- src/main/kotlin/com/example/todo/TodoApplication.kt
+++ src/main/kotlin/com/example/todo/TodoApplication.kt
@@ -2,18 +2,19 @@ package com.example.todo
 
 import com.example.todo.repository.TodoRepository
 import com.example.todo.domain.TodoDTO
-import com.example.todo.domain.toDTO
 import com.example.todo.domain.toEntity
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.ApplicationContext
 import java.time.LocalDate
-
+import com.example.todo.controller.TodoListController
 @SpringBootApplication
 class TodoApplication {
 	@Autowired
 	lateinit var repository: TodoRepository
+	@Autowired
+	lateinit var todoListController: TodoListController
 	fun run() {
 		val todoDTO= TodoDTO(
 			id = 1,
@@ -23,18 +24,23 @@ class TodoApplication {
 			isCompleted = false
 		)
 
-		val createdTodo = repository.save(todoDTO.toEntity()).toDTO()
+		todoListController.createTodo(todoDTO)
 
-		if (createdTodo != null) {
-			println("Created Todo: ${createdTodo.name}")
-		} else {
-			println("Failed to create Todo.")
-		}
+		val todoDTO2= TodoDTO(
+			id = 1,
+			name = "Create a Spring Todo Project",
+			description = "Follow the steps in the tutorial to create a Spring Todo project.",
+			dueDate = LocalDate.now().plusDays(7),
+			isCompleted = true
+		)
+
+		todoListController.updateTodoById(1, todoDTO2)
+		repository.save(todoDTO2.toEntity())
 
 		val allTodos = repository.findAll()
 
 		allTodos.forEach {
-			println("Todo: ${it.name}")
+			println("Todo: ${it.isCompleted}")
 		}
 	}
 }
@@ -44,4 +50,3 @@ fun main(args: Array<String>) {
 	val app = context.getBean(TodoApplication::class.java)
 	app.run()
 }
-
